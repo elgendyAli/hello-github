e327b3235dd880ad32beed891a39cd35d51781c1
Ali Elgendy <130915894+elgendyAli@users.noreply.github.com>
2023-05-05 01:22:04 +0000

    loops

---
+++ mario.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void) 
+{
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    } 
+    while (n < 1);
+    
+    const int n = 3;
+     for (int j = 0; j < n; j++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
